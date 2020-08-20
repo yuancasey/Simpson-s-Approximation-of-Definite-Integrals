df315d2a1e285d3c42db3a4314d2cc2c23a39db5
yuancasey <61532202+yuancasey@users.noreply.github.com>
2020-08-20 00:25:09 -0400

    Update simpsons.py

--- simpsons.py
+++ simpsons.py
@@ -1,18 +1,9 @@
 #******************************************************************************
 # simpsons.py
 #******************************************************************************
-# Name: Cheng (Casey) Yuan
+# Name: Casey Yuan
 #******************************************************************************
-# Collaborators/outside sources used 
-#(IMPORTANT! Write "NONE" if none were used):
-#NONE
-#
-#
-# Reminder: you are to write your own code.
-#******************************************************************************
-# Overall notes (not to replace inline comments):
-#
-#
+
 import math
 
 A = int(input("Enter A: "))
